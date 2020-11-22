7d56f2b3981debc74b51f4d997a4a329299dd253
VeremchukStanislav <verenchuk.stanislav@gmail.com>
2020-11-22 10:21:36 +0200

    готово

--- index.js
+++ index.js
@@ -55,3 +55,45 @@ function openJsLightbox(e) {
     ref.lightboxImage.alt = imgALT;
     ref.lightboxImage.dataset.index = index;
 }
+
+// Закрытие модалки по клику на крестик
+// Закрытие модалки по клику на устоту
+
+ref.lightboxButton.addEventListener('click', closeJsLightbox);
+ref.lightboxOverlay.addEventListener('click', closeJsLightbox);
+
+function closeJsLightbox() {
+    ref.jsLightbox.classList.remove("is-open");
+    ref.lightboxImage.src = '';
+    ref.lightboxImage.alt = '';
+}
+
+// Закрытие модалки по ESC.
+
+document.addEventListener('keydown', closeJsLightboxByKey);
+
+function closeJsLightboxByKey(e) {
+    if (e.code  === "Escape") {
+        closeJsLightbox();
+    }
+}
+
+// Листание картинок кнопками "влево" и "вправо".
+
+document.addEventListener('keydown', (e) => scrollGallery(e, galleryItems));
+
+function scrollGallery(e, gallery) {
+    let index = parseInt(ref.lightboxImage.dataset.index);
+    if (e.code === "ArrowLeft" && index > 0) {
+        index -= 1;
+        ref.lightboxImage.dataset.index = index;
+        ref.lightboxImage.src = gallery[index].original;
+        ref.lightboxImage.alt = gallery[index].description;
+    }
+    if (e.code === "ArrowRight" && index < gallery.length - 1) { 
+        index += 1;
+        ref.lightboxImage.dataset.index = index;
+        ref.lightboxImage.src = gallery[index].original;
+        ref.lightboxImage.alt = gallery[index].description;
+    }
+}
